cdab43c14291c88c3d83d2b088c5047c05287663
chillar85 <chillar85@gmx.net>
2020-06-03 11:25:07 +0200

    added Collision

---
+++ SpaceShooter/src/application/CollisionManager.java
@@ -0,0 +1,16 @@
+package application;
+
+public class CollisionManager {
+
+	public CollisionManager() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public boolean checkBoundsProjectile(Projectile p, Enemy e) {
+		return p.getBoundsInParent().intersects(e.getBoundsInParent());
+	}
+	
+	public boolean checkBoundsPlayer(Player p, Enemy e) {
+		return p.getBoundsInParent().intersects(e.getBoundsInParent());
+	}
+}
